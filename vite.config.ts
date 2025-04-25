7e1d3296f6fa903315fbb63e3fcacea363561c0b
Ladislav Zahnas <ladizala@gmail.com>
2025-04-25 19:59:33 +0200

    fix: edit base path and deploy script

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	base: '/bike-fit-compare' // Temporarily set to root for local testing
 });
